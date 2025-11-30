5e1eb8f6bf954cee3c60af754f49eecd5e36db20
Alessandro <al.belotti@reply.it>
2025-11-30 16:38:21 +0100

    bug fixing

--- src/retrieval/rag_engine.py
+++ src/retrieval/rag_engine.py
@@ -6,20 +6,10 @@ from openai import OpenAI
 load_dotenv(override=True)
 
 class RAG:
-    def __init__(self, retriever):  
-        client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-
-        response = client.chat.completions.create(
-            model=os.getenv("AZURE_OPENAI_DEPLOYMENT_NAME"),
-            messages=[
-                {"role": "system", "content": "You are a helpful assistant."},
-                {"role": "user", "content": "Explain quantum computing in simple terms"},
-            ]
-        )
-
-        print(response.choices[0].message["content"])
+    def __init__(self, retriever): 
 
         self.llm = self._setup_llm()
+        self.llm_name = os.getenv("OPENAI_DEPLOYMENT_NAME")
         self.retriever = retriever
 
         self.conversation_history = []
